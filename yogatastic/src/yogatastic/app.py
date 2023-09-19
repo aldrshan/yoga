ffe40b60deae91616c8db3ebb57772634d7b3fdb
Shane Aldridge <shane.d.aldridge@gmail.com>
2023-09-19 07:30:16 -0400

    Beeware work

--- yogatastic/src/yogatastic/app.py
+++ yogatastic/src/yogatastic/app.py
@@ -1,27 +1,36 @@
-"""
-A simple yoga app
-"""
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
-
-class YogaTastic(toga.App):
-
+class YogaApp(toga.App):
     def startup(self):
-        """
-        Construct and show the Toga application.
+        # Create the main box container
+        main_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        # Image view for the yoga pose
+        self.pose_image = toga.ImageView('path_to_initial_image.png')
+        main_box.add(self.pose_image)
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        # Dropdown for workout selection
+        self.workout_dropdown = toga.Selection(items=['Workout 1', 'Workout 2', ...])
+        main_box.add(self.workout_dropdown)
+
+        # Start/Pause button
+        self.start_button = toga.Button('Start', on_press=self.start_workout)
+        main_box.add(self.start_button)
+
+        # Countdown timer label
+        self.timer_label = toga.Label('01:00')
+        main_box.add(self.timer_label)
+
+        # Set the main window content
+        self.main_window = toga.MainWindow(title="Yoga App")
         self.main_window.content = main_box
         self.main_window.show()
 
+    def start_workout(self, widget):
+        # Logic to start the workout, update the timer, etc.
+        pass
 
 def main():
-    return YogaTastic()
+    return YogaApp()
